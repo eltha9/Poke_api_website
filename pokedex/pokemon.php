54a1e5f3e856fbde2adb6a2b05bba86b958170f5
eltha9 <theadosto@gmail.com>
2019-02-28 19:46:44 +0100

    add pokemon view stat

--- pokedex/pokemon.php
+++ pokedex/pokemon.php
@@ -7,7 +7,16 @@ if(!empty($_GET)){
     $data = to_curl('https://pokeapi.co/api/v2/pokemon/'.$pokemon);
     $data = json_decode($data);
 }else{
-    header('location: error.html');
+    // header('location: ./error.html');
+}
+
+function id_print($id){
+    if($id <10){
+        $id = '00'.$id;
+    }elseif($id <100){
+        $id = '00'.$id;
+    }
+    return $id;
 }
 ?>
 
@@ -17,14 +26,52 @@ if(!empty($_GET)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pokédex-<?= $data->name ?></title>
+    <link rel="stylesheet" href="styles/pokemon.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Space+Mono:400,700&amp;subset=latin-ext,vietnamese" rel="stylesheet">
 </head>
 <body>
-    <header>
-
+<header>
+        <a href="../index.html" title="home"><img src="http://cdn.elph.fr/logo-6221638601ef7fa7c835eae08ef67a16.png" alt="pokéapi games"></a>
     </header>
     <main>
-        
-    </main>
+        <section class="data-numbers">
+            <table>
+                <tr>
+                    <th>N° <?=id_print($data->id) ?></th>
+                    <td><?= $data->name?></td>   
+                </tr>
+                
+                <tr>
+                    <th>Hauteur</th>
+                    <td><?=$data->height/10 ?> m</td>   
+                </tr>
+                <tr>
+                    <th>Poids</th>
+                    <td><?=$data->weight/10 ?> kg</td>   
+                </tr>
+            </table>
+            <img src="<?= $data->sprites->front_default?>" alt="front <?=$data->name ?>">
+        </section>
+        <section class="data-type">
+            <div class="types">
+                <h2>Type</h2>
+                <?php
+                    foreach($data->types as $type){
+                        echo $type->type->name.'</br>';
+                    }
+                ?>       
+            </div>
+
+            <div class="abilities">
+                <h2>Abilities</h2>
+                <?php
+                    foreach($data->abilities as $ability){
+                        echo $ability->ability->name.'</br>';
+                    }
+                ?>
+            </div>
+        </section>
 </body>
 </html>
